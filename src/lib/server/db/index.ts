5dabfea417898bda30a045269a9a9816ec41af53
Marlena Mąkosza <marlenamakosza@gmail.com>
2025-12-06 18:14:54 +0100

    new age

--- src/lib/server/db/index.ts
+++
@@ -1,9 +0,0 @@
-import { drizzle } from "drizzle-orm/postgres-js";
-import postgres from "postgres";
-import * as schema from "./schema";
-
-const DATABASE_URL = process.env.DATABASE_URL || "postgresql://crm_user:crm_password@localhost:5432/crm_db";
-
-const client = postgres(DATABASE_URL, { prepare: false });
-
-export const db = drizzle(client, { schema });
